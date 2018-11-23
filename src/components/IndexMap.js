9c2cd37b0dd186ea98bd628ccbd6b371c895bab1
hungrated <zhang295415658@qq.com>
2018-11-23 11:17:30 +0800

    master: add map demo

--- src/components/IndexMap.js
+++ src/components/IndexMap.js
@@ -1,13 +1,183 @@
 import React from 'react';
 import styles from '../styles/IndexMap.less';
-// import ReactEcharts from 'echarts-for-react';
-// import 'echarts';
+import ReactEcharts from 'echarts-for-react';
+import 'echarts/extension/bmap/bmap';
+import 'echarts-gl';
 
 const IndexMap = ({data}) => {
 
+  const getOption = () => {
+    const getAirportCoord = (idx) => [data.airports[idx][3], data.airports[idx][4]];
+
+    const routes = data.routes.map(function (airline) {
+      return [
+        getAirportCoord(airline[1]),
+        getAirportCoord(airline[2])
+      ];
+    });
+
+    return {
+      bmap: {
+        center: [110, 33],
+        zoom: 5,
+        roam: true,
+        mapStyle: {
+          'styleJson': [
+            {
+              'featureType': 'water',
+              'elementType': 'all',
+              'stylers': {
+                'color': '#031628'
+              }
+            },
+            {
+              'featureType': 'land',
+              'elementType': 'geometry',
+              'stylers': {
+                'color': '#000102'
+              }
+            },
+            {
+              'featureType': 'highway',
+              'elementType': 'all',
+              'stylers': {
+                'visibility': 'off'
+              }
+            },
+            {
+              'featureType': 'arterial',
+              'elementType': 'geometry.fill',
+              'stylers': {
+                'color': '#000000'
+              }
+            },
+            {
+              'featureType': 'arterial',
+              'elementType': 'geometry.stroke',
+              'stylers': {
+                'color': '#0b3d51'
+              }
+            },
+            {
+              'featureType': 'local',
+              'elementType': 'geometry',
+              'stylers': {
+                'color': '#000000'
+              }
+            },
+            {
+              'featureType': 'railway',
+              'elementType': 'geometry.fill',
+              'stylers': {
+                'color': '#000000'
+              }
+            },
+            {
+              'featureType': 'railway',
+              'elementType': 'geometry.stroke',
+              'stylers': {
+                'color': '#08304b'
+              }
+            },
+            {
+              'featureType': 'subway',
+              'elementType': 'geometry',
+              'stylers': {
+                'lightness': -70
+              }
+            },
+            {
+              'featureType': 'building',
+              'elementType': 'geometry.fill',
+              'stylers': {
+                'color': '#000000'
+              }
+            },
+            {
+              'featureType': 'all',
+              'elementType': 'labels.text.fill',
+              'stylers': {
+                'color': '#857f7f'
+              }
+            },
+            {
+              'featureType': 'all',
+              'elementType': 'labels.text.stroke',
+              'stylers': {
+                'color': '#000000'
+              }
+            },
+            {
+              'featureType': 'building',
+              'elementType': 'geometry',
+              'stylers': {
+                'color': '#022338'
+              }
+            },
+            {
+              'featureType': 'green',
+              'elementType': 'geometry',
+              'stylers': {
+                'color': '#062032'
+              }
+            },
+            {
+              'featureType': 'boundary',
+              'elementType': 'all',
+              'stylers': {
+                'color': '#465b6c'
+              }
+            },
+            {
+              'featureType': 'manmade',
+              'elementType': 'all',
+              'stylers': {
+                'color': '#022338'
+              }
+            },
+            {
+              'featureType': 'label',
+              'elementType': 'all',
+              'stylers': {
+                'visibility': 'off'
+              }
+            }
+          ]
+        }
+      },
+      series: [{
+        type: 'lines',
+
+        coordinateSystem: 'bmap',
+
+        effect: {
+          show: true,
+          trailWidth: 0.01,
+          trailOpacity: 0.1,
+          trailLength: 0.01,
+          constantSpeed: 7
+        },
+
+        blendMode: 'lighter',
+
+        lineStyle: {
+          width: 0.3,
+          opacity: 0.03
+        },
+
+        data: routes
+      }]
+    };
+  };
+
   return (
     <div className={styles['g-map']}>
-      map
+      <ReactEcharts
+        option={getOption()}
+        notMerge={true}
+        lazyUpdate={true}
+        style={{width: '100%', height:'calc(100vh - 70px)'}}
+      />
     </div>
   );
 };
--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -4,12 +4,6 @@ import styles from '../styles/IndexPage.less';
 
 import IndexMap from '../components/IndexMap';
 
-// const mapStateToProps = (state) => {
-//   return {
-//     data: state.data
-//   };
-// };
-
 const mapStateToProps = ({ index }) => ({
   index,
 });
@@ -24,34 +18,16 @@ const mapDispatchToProps = dispatch => ({
 });
 
 class IndexPage extends PureComponent {
-  // state = {
-  //   data: {
-  //     airportsFields: [],
-  //     airlineFields: [],
-  //     airports: [],
-  //     airlines: [],
-  //     routes: []
-  //   }
-  // };
 
   componentDidMount () {
-    // this.props.dispatch({
-    //   type: 'index/fetch',
-    //   payload: {}
-    // });
     this.props.dispatcher.index.fetch();
   }
 
   render () {
-    // console.log(this.state);
     const { index: { data } } = this.props;
-    console.log('data is', data);
     return (
       <div className={styles['g-main']}>
         <IndexMap data={data}/>
-        <p className={styles['m-slogan']}>GCNT-Vis: Visualization and analysis
-          of global core network topology
-          information.</p>
       </div>
     );
   }

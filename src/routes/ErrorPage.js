7f7dd412a5bcc21ea10193b77218a7b11bbefbb1
hungrated <zhang295415658@qq.com>
2018-11-26 16:03:29 +0800

    master: modify error page

--- src/routes/ErrorPage.js
+++ src/routes/ErrorPage.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { connect } from 'dva';
-// import styles from '../styles/ErrorPage.less';
+import styles from '../styles/ErrorPage.less';
 
 function ErrorPage () {
   return (
-    <div>
-      Error
+    <div className={styles['g-main']}>
+      <img className={styles['m-img']}
+           src={require('../assets/gcnt-logo-s.png')} alt={'logo'}/>
+      <p className={styles['m-tlt']}>Oops, the page you request is
+        missing...</p>
+      <p className={styles['m-subtlt']}>Please try again.</p>
     </div>
   );
 }
